3e4dba796ae963121c2c0c3caab83caa401979ef
thchennai <hsinfotechs1@gmail.com>
2022-11-29 13:45:35 +0530

    success

---
+++ Cucumber_Report/report.js
@@ -0,0 +1,361 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Adactin.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Adactin Hotel Booking Application",
+  "description": "",
+  "id": "adactin-hotel-booking-application",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 2,
+  "name": "Login Fanctionality",
+  "description": "",
+  "id": "adactin-hotel-booking-application;login-fanctionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 3,
+  "name": "user Lunch The Adactin Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "user Enter The User Name From The DropBox",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "user Enter The Password From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "user Click The LoginBtn And Navigate To Search Hotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Lunch_The_Adactin_Application()"
+});
+formatter.result({
+  "duration": 14971160800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enter_The_User_Name_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1149732800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enter_The_Password_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 857932500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Click_The_LoginBtn_And_Navigate_To_Search_Hotel_Page()"
+});
+formatter.result({
+  "duration": 5765953200,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "Search Fanctionality",
+  "description": "",
+  "id": "adactin-hotel-booking-application;search-fanctionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "user Select The Location From The DropBox",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "user Select The Hotels From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user Select The Room Type From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "user Select The NumberOfRooms From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "user Select The CheckInDate From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "user Select The CheckOutDate From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "user Select The NumberOfAdults From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "user Select The NumberOfChildren From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "userClick The SearchBtn And Navigate to SelectHotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_Location_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1423593900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_Hotels_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1327200800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_Room_Type_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1089833800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_NumberOfRooms_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1168462200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_CheckInDate_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1406700000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_CheckOutDate_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1382046600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_NumberOfAdults_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1848615000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_NumberOfChildren_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1263820300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.userclick_The_SearchBtn_And_Navigate_to_SelectHotel_Page()"
+});
+formatter.result({
+  "duration": 8520073700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 19,
+  "name": "Select Fanctionality",
+  "description": "",
+  "id": "adactin-hotel-booking-application;select-fanctionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 20,
+  "name": "user Click The RadioBtn",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 21,
+  "name": "user Click The Continue And Navigate To BookHotel Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Click_The_RadioBtn()"
+});
+formatter.result({
+  "duration": 1081842700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Click_The_Continue_And_Navigate_To_BookHotel_Page()"
+});
+formatter.result({
+  "duration": 10851424700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "Booking Fanctionality",
+  "description": "",
+  "id": "adactin-hotel-booking-application;booking-fanctionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 24,
+  "name": "user Enthe The FirstName From The DropBox",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 25,
+  "name": "user Enthe The LastName From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "user Enthe The BillingAdd From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "user Enthe The CreditCardNumber From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "user Enthe The FirstName From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "user Select The CardType From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "user Select The Card DateAndMonth From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "user Select The Card DateAndYear From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "user Enthe The CvvNumber From The DropBox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "user Click The BookNowBtn And Navigate to Booking Confirmation Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enthe_The_FirstName_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1629603300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enthe_The_LastName_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1867227200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enthe_The_BillingAdd_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1150914700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enthe_The_CreditCardNumber_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1895931400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enthe_The_FirstName_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1010656500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_CardType_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1614577800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_Card_DateAndMonth_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 949564500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Select_The_Card_DateAndYear_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1294872300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Enthe_The_CvvNumber_From_The_DropBox()"
+});
+formatter.result({
+  "duration": 1135317700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Click_The_BookNowBtn_And_Navigate_to_Booking_Confirmation_Page()"
+});
+formatter.result({
+  "duration": 7015280600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 35,
+  "name": "Booking Confirmation Fanctionality",
+  "description": "",
+  "id": "adactin-hotel-booking-application;booking-confirmation-fanctionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 36,
+  "name": "user Click On The SearchHotelBtn",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Adactin_StepDef.user_Click_On_The_SearchHotelBtn()"
+});
+formatter.result({
+  "duration": 8960412500,
+  "status": "passed"
+});
+});
